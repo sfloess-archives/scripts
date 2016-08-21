22f9534c50bfb7c4d44b8f9b56590161b48943de
Scot P. Floess <sfloess@redhat.com>
2016-08-21 10:53:35 -0400

    Had some bad params for auth and made ksmetas variables so I was not duplicating values over and over

--- bash/cobbler.sh
+++ bash/cobbler.sh
@@ -3,8 +3,8 @@
 . `dirname ${BASH_SOURCE[0]}`/../scripts/bash/cobbler-utils.sh
 
 addDistros() {
-    ENTERPISE_KSMETA_DATA="auth=-useshadow,--enablemd5,--enablenis,--nisdomain=flossware.com packages=koan,redhat-lsb"
-    FEDORA_KSMETA_DATA="auth=-useshadow,--enablemd5,--enablenis,--nisdomain=flossware.com p0 packages=koan"
+    ENTERPISE_KSMETA_DATA='auth="--useshadow --enablenis --nisdomain=flossware.com" packages=koan,redhat-lsb'
+    FEDORA_KSMETA_DATA='auth="--useshadow --enablenis --nisdomain=flossware.com" p0 packages=koan"'
 
     distro-add        CentOS-7-x86_64          /root/distro/iso/CentOS-5.11-x86_64-bin-DVD-1of2.iso     --ksmeta="${ENTERPISE_KSMETA_DATA}"
     distro-add        CentOS-7-x86_64          /root/distro/iso/CentOS-6.8-x86_64-bin-DVD1.iso          --ksmeta="${ENTERPISE_KSMETA_DATA}"
@@ -70,27 +70,29 @@ addProfiles() {
 }
 
 addHosts() {
-    cobbler-exec system add --name="host-1" --hostname="host-1" --profile="CentOS-7-x86_64" --interface="eth0" --mac-address="00:14:22:2A:AF:F8" --virt-type="xenpv" --ksmeta='auth="-useshadow --enablenis --nisdomain=flossware.com" packages=koan,redhat-lsb lvmDisks=sda,sdb'
+    cobbler-exec system add --name="host-1" --hostname="host-1" --profile="CentOS-7-x86_64" --interface="eth0" --mac-address="00:14:22:2A:AF:F8" --virt-type="xenpv" --ksmeta='auth="--useshadow --enablenis --nisdomain=flossware.com" packages=koan,redhat-lsb lvmDisks=sda,sdb'
 
-    cobbler-exec system add --name="host-2" --hostname="host-2" --profile="RHEL-7-x86_64" --interface="eth0" --mac-address="00:19:B9:1F:34:B6" --virt-type="kvm" --ksmeta='auth="-useshadow --enablenis --nisdomain=flossware.com" packages=koan,redhat-lsb lvmDisks=sda,sdb,sdc'
+    cobbler-exec system add --name="host-2" --hostname="host-2" --profile="RHEL-7-x86_64" --interface="eth0" --mac-address="00:19:B9:1F:34:B6" --virt-type="kvm" --ksmeta='auth="--useshadow --enablenis --nisdomain=flossware.com" packages=koan,redhat-lsb lvmDisks=sda,sdb,sdc'
 }
 
 addXenVms() {
-    cobbler-exec system add --name="centos-5-xen"  --hostname="centos-xen" --profile="CentOS-5-x86_64"  --interface="eth0"  --mac-address="random" --virt-type="xenpv" --virt-file-size="10" --virt-ram="2048" --virt-bridge="bridge0" --virt-cpus="4" --ksmeta='bootloader="--location=mbr --boot-drive=xvda"'
-    cobbler-exec system add --name="centos-6-xen"  --hostname="centos-xen" --profile="CentOS-6-x86_64"  --interface="eth0"  --mac-address="random" --virt-type="xenpv" --virt-file-size="10" --virt-ram="2048" --virt-bridge="bridge0" --virt-cpus="4" --ksmeta='bootloader="--location=mbr --boot-drive=xvda"'
-    cobbler-exec system add --name="centos-7-xen"  --hostname="centos-xen" --profile="CentOS-7-x86_64"  --interface="eth0"  --mac-address="random" --virt-type="xenpv" --virt-file-size="10" --virt-ram="2048" --virt-bridge="bridge0" --virt-cpus="4" --ksmeta='bootloader="--location=mbr --boot-drive=xvda"'
-    cobbler-exec system add --name="fedora-24-xen" --hostname="fedora-xen" --profile="Fedora-24-x86_64" --interface="eth0"  --mac-address="random" --virt-type="xenpv" --virt-file-size="10" --virt-ram="2048" --virt-bridge="bridge0" --virt-cpus="4" --ksmeta='bootloader="--location=mbr --boot-drive=xvda"'
-    cobbler-exec system add --name="rhel-7-xen"    --hostname="rhel-xen"   --profile="RHEL-7-x86_64"    --interface="eth0"  --mac-address="random" --virt-type="xenpv" --virt-file-size="10" --virt-ram="2048" --virt-bridge="bridge0" --virt-cpus="4" --ksmeta='bootloader="--location=mbr --boot-drive=xvda"'
+    XEN_VM_KSMETA_DATA='bootloader="--location=mbr --boot-drive=xvda"'
 
-    cobbler-exec system add --name="centos-7-atomic-xen"  --hostname="centos-atomic-xen" --profile="CentOS-7-Atomic-x86_64"   --interface="eth0"  --mac-address="random" --virt-type="xenpv" --virt-file-size="10" --virt-ram="2048" --virt-bridge="bridge0" --ksmeta='bootloader="--location=mbr --boot-drive=xvda"'
-    cobbler-exec system add --name="fedora-24-atomic-xen" --hostname="fedora-atomic-xen" --profile="Fedora-24-Atomic-x86_64"  --interface="eth0"  --mac-address="random" --virt-type="xenpv" --virt-file-size="10" --virt-ram="2048" --virt-bridge="bridge0" --ksmeta='bootloader="--location=mbr --boot-drive=xvda"'
-    cobbler-exec system add --name="rhel-7-atomic-xen"    --hostname="rhel-atomic-xen"   --profile="RHEL-7-Atomic-x86_64"     --interface="eth0"  --mac-address="random" --virt-type="xenpv" --virt-file-size="10" --virt-ram="2048" --virt-bridge="bridge0" --ksmeta='bootloader="--location=mbr --boot-drive=xvda"'
+    cobbler-exec system add --name="centos-5-xen"  --hostname="centos-xen" --profile="CentOS-5-x86_64"  --interface="eth0"  --mac-address="random" --virt-type="xenpv" --virt-file-size="10" --virt-ram="2048" --virt-bridge="bridge0" --virt-cpus="4" --ksmeta=${XEN_VM_KSMETA_DATA}
+    cobbler-exec system add --name="centos-6-xen"  --hostname="centos-xen" --profile="CentOS-6-x86_64"  --interface="eth0"  --mac-address="random" --virt-type="xenpv" --virt-file-size="10" --virt-ram="2048" --virt-bridge="bridge0" --virt-cpus="4" --ksmeta=${XEN_VM_KSMETA_DATA}
+    cobbler-exec system add --name="centos-7-xen"  --hostname="centos-xen" --profile="CentOS-7-x86_64"  --interface="eth0"  --mac-address="random" --virt-type="xenpv" --virt-file-size="10" --virt-ram="2048" --virt-bridge="bridge0" --virt-cpus="4" --ksmeta=${XEN_VM_KSMETA_DATA}
+    cobbler-exec system add --name="fedora-24-xen" --hostname="fedora-xen" --profile="Fedora-24-x86_64" --interface="eth0"  --mac-address="random" --virt-type="xenpv" --virt-file-size="10" --virt-ram="2048" --virt-bridge="bridge0" --virt-cpus="4" --ksmeta=${XEN_VM_KSMETA_DATA}
+    cobbler-exec system add --name="rhel-7-xen"    --hostname="rhel-xen"   --profile="RHEL-7-x86_64"    --interface="eth0"  --mac-address="random" --virt-type="xenpv" --virt-file-size="10" --virt-ram="2048" --virt-bridge="bridge0" --virt-cpus="4" --ksmeta=${XEN_VM_KSMETA_DATA}
 
-    cobbler-exec system add --name="workstation"   --hostname="workstation"   --profile="RHEL-7-x86_64"        --interface="eth0"  --mac-address="00:16:3e:1c:97:ac" --virt-type="xenpv" --virt-file-size="20"  --virt-ram="2048" --virt-bridge="bridge0" --virt-cpus=4 --ksmeta='bootloader="--location=mbr --boot-drive=xvda"'
-    cobbler-exec system add --name="atomic-master" --hostname="atomic-master" --profile="RHEL-7-Atomic-x86_64" --interface="eth0"  --mac-address="00:16:3e:39:ce:5e" --virt-type="xenpv" --virt-file-size="100" --virt-ram="2048" --virt-bridge="bridge0" --virt-cpus=1 --ksmeta='bootloader="--location=mbr --boot-drive=xvda"'
-    cobbler-exec system add --name="db"            --hostname="db"            --profile="RHEL-7-x86_64"        --interface="eth0"  --mac-address="00:16:3e:50:39:07" --virt-type="xenpv" --virt-file-size="100" --virt-ram="2048" --virt-bridge="bridge0" --virt-cpus=1 --ksmeta='bootloader="--location=mbr --boot-drive=xvda"'
-    cobbler-exec system add --name="pulp"          --hostname="pulp"          --profile="RHEL-7-x86_64"        --interface="eth0"  --mac-address="00:16:3e:4e:fc:64" --virt-type="xenpv" --virt-file-size="100" --virt-ram="2048" --virt-bridge="bridge0" --virt-cpus=4 --ksmeta='bootloader="--location=mbr --boot-drive=xvda"'
-    cobbler-exec system add --name="solr"          --hostname="solr"          --profile="RHEL-7-x86_64"        --interface="eth0"  --mac-address="00:16:3e:7f:38:c8" --virt-type="xenpv" --virt-file-size="100" --virt-ram="3400" --virt-bridge="bridge0" --virt-cpus=4 --ksmeta='bootloader="--location=mbr --boot-drive=xvda"'
+    cobbler-exec system add --name="centos-7-atomic-xen"  --hostname="centos-atomic-xen" --profile="CentOS-7-Atomic-x86_64"   --interface="eth0"  --mac-address="random" --virt-type="xenpv" --virt-file-size="10" --virt-ram="2048" --virt-bridge="bridge0" --ksmeta=${XEN_VM_KSMETA_DATA}
+    cobbler-exec system add --name="fedora-24-atomic-xen" --hostname="fedora-atomic-xen" --profile="Fedora-24-Atomic-x86_64"  --interface="eth0"  --mac-address="random" --virt-type="xenpv" --virt-file-size="10" --virt-ram="2048" --virt-bridge="bridge0" --ksmeta=${XEN_VM_KSMETA_DATA}
+    cobbler-exec system add --name="rhel-7-atomic-xen"    --hostname="rhel-atomic-xen"   --profile="RHEL-7-Atomic-x86_64"     --interface="eth0"  --mac-address="random" --virt-type="xenpv" --virt-file-size="10" --virt-ram="2048" --virt-bridge="bridge0" --ksmeta=${XEN_VM_KSMETA_DATA}
+
+    cobbler-exec system add --name="workstation"   --hostname="workstation"   --profile="RHEL-7-x86_64"        --interface="eth0"  --mac-address="00:16:3e:1c:97:ac" --virt-type="xenpv" --virt-file-size="20"  --virt-ram="2048" --virt-bridge="bridge0" --virt-cpus=4 --ksmeta=${XEN_VM_KSMETA_DATA}
+    cobbler-exec system add --name="atomic-master" --hostname="atomic-master" --profile="RHEL-7-Atomic-x86_64" --interface="eth0"  --mac-address="00:16:3e:39:ce:5e" --virt-type="xenpv" --virt-file-size="100" --virt-ram="2048" --virt-bridge="bridge0" --virt-cpus=1 --ksmeta=${XEN_VM_KSMETA_DATA}
+    cobbler-exec system add --name="db"            --hostname="db"            --profile="RHEL-7-x86_64"        --interface="eth0"  --mac-address="00:16:3e:50:39:07" --virt-type="xenpv" --virt-file-size="100" --virt-ram="2048" --virt-bridge="bridge0" --virt-cpus=1 --ksmeta=${XEN_VM_KSMETA_DATA}
+    cobbler-exec system add --name="pulp"          --hostname="pulp"          --profile="RHEL-7-x86_64"        --interface="eth0"  --mac-address="00:16:3e:4e:fc:64" --virt-type="xenpv" --virt-file-size="100" --virt-ram="2048" --virt-bridge="bridge0" --virt-cpus=4 --ksmeta=${XEN_VM_KSMETA_DATA}
+    cobbler-exec system add --name="solr"          --hostname="solr"          --profile="RHEL-7-x86_64"        --interface="eth0"  --mac-address="00:16:3e:7f:38:c8" --virt-type="xenpv" --virt-file-size="100" --virt-ram="3400" --virt-bridge="bridge0" --virt-cpus=4 --ksmeta=${XEN_VM_KSMETA_DATA}
 }
 
 addKvmVms() {
