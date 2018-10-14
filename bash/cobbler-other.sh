50503a4313a6a636b9ed4c3cc73beb45587a57b7
Scot P. Floess <sfloess@redhat.com>
2018-10-14 15:33:10 -0400

    NA - latest and most pristine

--- bash/cobbler-other.sh
+++ bash/cobbler-other.sh
@@ -2,6 +2,16 @@
 
 . `dirname ${BASH_SOURCE[0]}`/../flossware-scripts/bash/cobbler-utils.sh
 
-distro-add   Debian-9-x86_64  /root/distro/iso/debian-9.4.0-amd64-DVD-1.iso
+cobbler signature update
 
-cobbler-exec system add --name="debian"  --hostname="debian"  --profile="Debian-9-x86_64"  --interface="eth0"  --mac-address="random" --virt-type="kvm" --virt-file-size="25" --virt-ram="4096" --virt-bridge="bridge0"  --netboot-enabled=1
+distro-add Debian-9-x86_64  /root/distro/iso/debian-9.4.0-amd64-DVD-1.iso
+distro-add Ubuntu-14-x86_64 /root/distro/iso/ubuntu-14.04.5-server-amd64.iso
+
+cobbler-exec profile add --name="Debian-9-x86_64"  --distro="Debian-9-x86_64"
+cobbler-exec profile add --name="Ubuntu-14-x86_64" --distro="Ubuntu-14-x86_64"
+
+cobbler-exec system add --name="debian"  --hostname="debian"  --profile="Debian-9-x86_64"   --interface="eth0"  --mac-address="random" --virt-type="kvm" --virt-file-size="25" --virt-ram="4096" --virt-bridge="bridge0"  --netboot-enabled=1
+cobbler-exec system add --name="ubuntu"  --hostname="ubuntu"  --profile="Ubuntu-14-x86_64"  --interface="eth0"  --mac-address="random" --virt-type="kvm" --virt-file-size="25" --virt-ram="4096" --virt-bridge="bridge0"  --netboot-enabled=1
+
+cobbler-exec system add --name="debian-xen"  --hostname="debian-xen"  --profile="Debian-9-x86_64"   --interface="eth0"  --mac-address="random" --virt-type="xen" --virt-file-size="25" --virt-ram="4096" --virt-bridge="bridge0"  --netboot-enabled=1
+cobbler-exec system add --name="ubuntu-xen"  --hostname="ubuntu-xen"  --profile="Ubuntu-14-x86_64"  --interface="eth0"  --mac-address="random" --virt-type="xen" --virt-file-size="25" --virt-ram="4096" --virt-bridge="bridge0"  --netboot-enabled=1
--- bash/xen-cobbler.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. `dirname ${BASH_SOURCE[0]}`/../flossware-scripts/bash/cobbler-utils.sh
-
-STANDARD_KICKSTART="/var/lib/cobbler/kickstarts/flossware_standard.ks"
-
-cobbler-exec system remove --name="host-1"
-#cobbler-exec system add    --name="host-1" --hostname="host-1" --profile="Fedora-27-x86_64" --interface="eth0" --mac-address="00:14:22:2A:AF:F8" --virt-type="xenpv" --ksmeta='lvmDisks="sda sdb"'
-cobbler-exec system add    --name="host-1" --hostname="host-1" --profile="Fedora-27-x86_64" --interface="eth0" --mac-address="00:14:22:2A:AF:F8" --virt-type="xenpv" --ksmeta='lvmDisks="sda sdb" packages=""' --kickstart="${STANDARD_KICKSTART}"
